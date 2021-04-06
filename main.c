14ad345a9fa570d3ffc939913172ba7592921129
Ricardo Oliveira <ricardo-cadastros@hotmail.com>
2021-04-05 22:48:49 -0300

    [feature] Valida a entrada segundo os tokens válidos.

--- main.c
+++ main.c
@@ -13,13 +13,11 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <stdbool.h>
 #define MAX_SIZE 1000
 
-char * read_string(); /* Lê uma string do teclado. */
-// char * rtrim(char *);
-// char * ltrim(char *);
-// char * trim(char *);
+char * readString(); /* Lê uma string do teclado. */
 
 /*
  * Struct: stack
@@ -35,30 +33,42 @@ struct Stack {
 struct Stack * createStack();      /* Cria uma nova pilha. */
 void push(struct Stack *, char *); /* Insere um novo elemento na pilha. */
 char * pop(struct Stack *);        /* Remove um elemento da pilha. */
-int isEmpty();                     /* Verifica se uma pilha está vazia. */
-int isFull(struct Stack *);        /* Verifica se uma pilha está cheia. */
-void printStack(struct Stack *);
+bool isEmptyStack(struct Stack *); /* Verifica se uma pilha está vazia. */
+bool isFullStack(struct Stack *);  /* Verifica se uma pilha está cheia. */
+void printStack(struct Stack *);   /* Imprime o conteúdo de uma pilha. */
+
+struct Queue {
+    int top;
+    int end;
+    int limit;
+    int nTokens;
+    char tokens[MAX_SIZE][MAX_SIZE];
+};
+
+struct Queue * createQueue();         /* Cria uma fila. */
+void enqueue(struct Queue *, char *); /* Insere um novo elemento na fila. */
+char * dequeue(struct Queue *);       /* Remove um elemento da fila. */
+bool isEmptyQueue(struct Queue *);    /* Verifica se uma fila está vazia. */
+bool isFullQueue(struct Queue *);     /* Verifica se uma fila está cheia. */
+void printQueue(struct Queue *);      /* Imprime o conteúdo de uma fila. */
+
 bool analisadorLexico(struct Stack *, char *);
 int getToken(char [], char[]);
-void putToken(char expression[], char token[]);
+void putToken(char [], char []);
+bool checkTokens(struct Queue *, char *);
+bool isValidToken(const char []);
+bool isOperator(const char []);
+bool isNumber(const char []);
+bool isCommand(const char []);
 
 int main()
 {
     char * expression;
-
-    expression = read_string();
+    expression = readString();
     printf("\n");
 
-    struct Stack * stack = createStack();
-
-    bool valido = analisadorLexico(stack, expression);
-
-    if(!valido) {
-        printf("\nEntrada invalida!\n");
-        free(expression);
-
-        return 1;
-    }
+    struct Queue * tokens = createQueue();
+    if(!checkTokens(tokens, expression)) return 1;
 
     printf("\nEntrada valida!\n");
     free(expression);
@@ -66,232 +76,93 @@ int main()
     return 0;
 }
 
-bool analisadorLexico(struct Stack * stack, char * expression) {
+// Varre a string, quebrando-a em tokens e inserindo-os numa fila
+bool checkTokens(struct Queue * tokens, char * expression) {
     char aux[(int)(strlen(expression)/sizeof(char))];
     char token[(int)(strlen(expression)/sizeof(char))];
     strcpy(aux, expression);
 
-    bool ultimoCaractereEOperadorBinario = false;
-    bool ultimoCaractereENumero = false;
-    bool primeiroCaractere = true;
-    bool segundoNumero = false;
-
-    bool expressaoValida = true;
-
     do {
-
-        if (getToken(aux, token) == 1) return expressaoValida;
-
-
-        if(strstr("0123456789", token)) {
-            printf("%s\n", token);
-
-        } else if(strstr(token, "enter")) {
-            printf("%s\n", token);
-            segundoNumero = !segundoNumero;
-
-        } else if(primeiroCaractere && strstr("-", token)) {
-
-            bool valido = false;
-
-            while(strstr("0123456789", token)) {
-                push(stack, token);
-                valido = true;
-
-                if (getToken(aux,  token) == 1) {
-                    printf("- => sinal\n");
-                    printStack(stack);
-                    return expressaoValida;
-                }
-            }
-
-            if(valido) {
-                printf("- => sinal\n");
-                printStack(stack);
-
-                putToken(aux, token);
-            } else {
-                expressaoValida = false;
-
-                printf("- => erro\n");
-                printStack(stack);
-
-                if (getToken(aux, token) == 1) return 1;
-
-                while(!strstr("enter", token)) {
-                    printf("%s\n", token);
-
-                    if (getToken(aux, token) == 1) return 1;
-                }
-
-                printf("%s\n", token);
-            }
-
-        } else if(strstr(".", token)) {
-
-            if (getToken(aux, token) == 1) {
-                printf("%s => erro\n", token);
-                return false;
-            }
-
-            if(!ultimoCaractereENumero && !primeiroCaractere) {
-                expressaoValida = false;
-
-                printf(". => erro\n");
-
-                while(!strstr("enter", token)) {
-                    printf("%s\n", token);
-
-                    if (getToken(aux, token) == 1) return expressaoValida;
-                }
-
-                printf("%s\n", token);
-            } else {
-
-                bool valido = false;
-
-                while(strstr("0123456789", token)) {
-                    push(stack, token);
-                    valido = true;
-
-                    if (getToken(aux,  token) == 1) {
-                        printf(". => ponto fluatuante\n");
-                        printStack(stack);
-                        return expressaoValida;
-                    }
-                }
-
-                if(valido) {
-                    printf(". => ponto fluatuante\n");
-                    printStack(stack);
-
-                    putToken(aux, token);
-                } else {
-                    expressaoValida = false;
-
-                    printf(". => erro\n");
-                    printStack(stack);
-
-                    while(!strstr("enter", token)) {
-                        printf("%s\n", token);
-
-                        if (getToken(aux, token) == 1) return expressaoValida;
-                    }
-
-                    printf("%s\n", token);
-                }
-            }
-
-        } else if(strstr(token, "^") || strstr(token, "E") || strstr(token, "e")) {
-
-            if (getToken(aux, token) == 1) {
-                printf("%s => erro\n", token);
-                return false;
-            }
-
-            char auxPotencia[(int)((strlen(token))/sizeof(char))];
-
-            memset(auxPotencia, 0, strlen(auxPotencia));
-
-            strcat(auxPotencia, token);
-
-            if(!ultimoCaractereENumero && !primeiroCaractere) {
-                expressaoValida = false;
-
-                printf(". => erro\n");
-
-                while(!strstr("enter", token)) {
-                    printf("%s\n", token);
-
-                    if (getToken(aux, token) == 1) return expressaoValida;
-                }
-
-                printf("%s\n", token);
-            } else {
-
-                bool valido = false;
-
-                while(strstr("0123456789", token)) {
-                    push(stack, token);
-
-                    valido = true;
-
-                    if (getToken(aux, token) == 1) {
-                        printf("%s => potencia\n", auxPotencia);
-                        printStack(stack);
-                        return expressaoValida;
-                    }
-                }
-
-                if(valido) {
-                    printf("%s => potencia\n", auxPotencia);
-
-                    printStack(stack);
-
-                    putToken(aux, token);
-                } else {
-                    printf("%s => erro\n", auxPotencia);
-
-                    printStack(stack);
-
-                    while(!strstr("enter", token)) {
-                        printf("%s\n", token);
-
-                        if (getToken(aux, token) == 1) return expressaoValida;
-                    }
-
-                    printf("%s\n", token);
-                }
-
-            }
-
-        } else if(strstr("+-*/", token)) {
-
-            if(segundoNumero && ultimoCaractereENumero) {
-                printf("%s => operador binario\n", token);
-            } else {
-                expressaoValida = false;
-
-                printf("%s => erro\n", token);
-
-                if (getToken(aux, token) == 1) return expressaoValida;
-
-                while(!strstr("enter", token)) {
-                    printf("%s\n", token);
-
-                    if (getToken(aux, token) == 1) return expressaoValida;
-                }
-
-                printf("%s\n", token);
-            }
-
-        } else if(strstr(token, "log") || strstr(token, "cos") || strstr(token, "sen")) {
-
-            if(ultimoCaractereENumero || ultimoCaractereEOperadorBinario) {
-                printf("%s => operador unario\n", token);
-            } else {
-                printf("%s => erro\n", token);
-
-                if (getToken(aux, token) == 1) return expressaoValida;
-
-                while(!strstr("enter", token)) {
-                    printf("%s\n", token);
-
-                    if (getToken(aux, token) == 1) return expressaoValida;
-                }
-
-                printf("%s\n", token);
-            }
+        if (getToken(aux, token) == 1){
+            printf("Insira tokens válidos!\n");
+            return false;
         }
 
-        ultimoCaractereENumero = strstr("0123456789.", token);
+        if (isValidToken(token)) {
+            enqueue(tokens, token);
+        } else {
+            printf("O token '%s' é inválido!\n", token);
+            return false;
+        }
+    } while(( (int)(strlen(aux)/sizeof(char)) ) > 0);
+    return true;
+}
 
-        ultimoCaractereEOperadorBinario = strstr("+-*/", token);
+// Verifica se o token é válido
+bool isValidToken(const char token[]) {
+    if(isNumber(token)) return true;
+    if(isOperator(token)) return true;
+    if(isCommand(token)) return true;
 
-        primeiroCaractere = strstr("enter", token);
-    } while((int)(strlen(aux)/sizeof(char)) > 0);
+    return false;
+}
 
-    return expressaoValida;
+// Verifica se o token é um número ou um dígito que compõe um número
+bool isNumber(const char token[]) {
+    if( ((int)(strlen(token) / sizeof(char))) > 3) return false;
+
+    char digit = (char)token[1];
+    return strchr("0123456789E.", digit) == NULL ? false : true;
+}
+
+// Verifica se o token é um operador
+bool isOperator(const char token[]) {
+    if( ((int)(strlen(token) / sizeof(char))) > 5) return false;
+
+    char operators[8][3] = {"+", "-", "*", "/", "^", "log", "sen", "cos"};
+    int i;
+
+    for(i = 0; i < 8; i++){
+        if(strlen(token) - 2 > strlen(operators[i])) continue;
+        if(strncasecmp(token + 1, operators[i], strlen(operators[i])) == 0) return true;
+    }
+
+    return false;
+}
+
+// Verificas se o token é um comando
+bool isCommand(const char token[]) {
+    if( ((int)(strlen(token) / sizeof(char))) > 7) return false;
+
+    char commands[1][5] = {"enter"};
+    int i;
+
+    for(i = 0; i < 1; i++){
+        if(strncasecmp(token + 1, commands[i], strlen(commands[i])) == 0) return true;
+    }
+
+    return false;
+}
+
+// Cria uma fila
+struct Queue * createQueue() {
+    struct Queue * queue = (struct Queue *)malloc(sizeof(struct Queue));
+    queue->top = 0;
+    queue->end = -1;
+    queue->nTokens = 0;
+    queue->limit = MAX_SIZE;
+
+    return queue;
+}
+
+// Insere um novo elemento na fila
+void enqueue(struct Queue * queue, char token[]) {
+    if(queue->end == queue->limit - 1)
+        queue->end = -1;
+
+    queue->end++;
+    strcpy(queue->tokens[queue->end], token);
+    queue->nTokens++;
 }
 
 int getToken(char expression[], char token[]) {
@@ -301,13 +172,13 @@ int getToken(char expression[], char token[]) {
     int endAt = (endAtPtr == NULL ? -1 : endAtPtr - expression);       // Índice para o primeiro '>'
 
     if (startAt != 0) return 1;
-    if (endAt == -1) return 1;
+    if (endAt == -1 || startAt == -1) return 1;
     if (startAt > endAt) return 1;
 
-    memset(token, 0, strlen(token));
+    memset(token, 0, strlen(token));     // Limpa o conteúdo do token
+    strncpy(token, expression, ++endAt); // Copia o token, sem incluir os '<' e '>'
 
-    strncpy(token, expression+startAt+1, ++endAt-2);
-
+    // Verifica se sobraram caractéres na expressão
     if ((int)(strlen(expression) / sizeof(char) - endAt) == 0) {
         strcpy(expression, "");
     } else {
@@ -319,30 +190,8 @@ int getToken(char expression[], char token[]) {
     return 0;
 }
 
-void putToken(char expression[], char token[]) {
-    char aux[(int)((strlen(expression)+strlen(token)+2)/sizeof(char))];
-
-    memset(aux, 0, strlen(aux));
-
-    strncat(aux, "<", (int)(strlen("<")/sizeof(char)));
-    strncat(aux, token, (int)(strlen(token)/sizeof(char)));
-    strncat(aux, ">", (int)(strlen(">")/sizeof(char)));
-
-    strncat(aux, expression, (int)(strlen(expression)/sizeof(char)));
-
-    memset(expression, 0, strlen(expression));
-
-    strcpy(expression, aux);
-}
-
-/*
- * Function: read_string
- * ---------------------
- * Lê uma sequência de caractéres do teclado e cria uma string.
- *
- * returns: um ponteiro para a string gerada.
- */
-char * read_string() {
+// Lê uma sequência de caractéres do teclado e cria uma string.
+char * readString() {
     char * str = NULL, ch;
     size_t size = 0;
     int i = 0;
@@ -370,25 +219,25 @@ struct Stack * createStack() {
 }
 
 // Verifica se a pilha está vazia
-int isEmpty(struct Stack * stack) {
+bool isEmptyStack(struct Stack * stack) {
     return stack->top == -1;
 }
 
 // Verifica se a pilha está cheia
-int isFull(struct Stack* stack) {
+bool isFullStack(struct Stack* stack) {
     return stack->top == stack->limit - 1;
 }
 
 // Insere um novo elemento na pilha
 void push(struct Stack * stack, char * token) {
-    if(!isFull(stack)) {
+    if(!isFullStack(stack)) {
         strcpy(stack->tokens[++stack->top], token);
     }
 }
 
 // Remove um elemento da pilha
 char * pop(struct Stack * stack) {
-    if(isEmpty(stack)) {
+    if(isEmptyStack(stack)) {
         printf("Pilha vazia.\n");
         return "";
     }
@@ -397,7 +246,7 @@ char * pop(struct Stack * stack) {
 }
 
 void printStack(struct Stack * stack) {
-    while(!isEmpty(stack)) {
+    while(!isEmptyStack(stack)) {
         printf("desimpilha: %s\n", pop(stack));
     }
 }
